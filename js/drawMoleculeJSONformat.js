4004cddb5ba7170bfbd627cb88ef26e4f35e20f6
brennanpincardiff <brennanpincardiff@gmail.com>
2014-09-11 13:42:02 +0100

    this script renders a molecule from a JSON object in d3 on a page.

---
+++ js/drawMoleculeJSONformat.js
@@ -0,0 +1,56 @@
+var paul=paul || {};
+
+paul.drawMoleculeJSONformat=function(strategy)
+{
+ // molecule is the JSON object that describes the parts of the protein. It encodes domains, start, end, info and color
+ 
+    var molecule=strategy.molecule;
+    var tooltipMethod=strategy.tooltip;
+    var place = strategy.place;
+
+ //   var CD40 = [
+ //       {"Name":"Topological domain", "start":21, "end": 193, "Info":"Extracellular Potential", "color":"blue"},
+ //       {"Name":"Transmembrane", "start":194, "end": 215, "Info":"Helical; Potential", "color":"green"},
+ //       {"Name":"Topological domain", "start":216, "end": 277, "Info":"Cytoplasmic Potential", "color":"red"},
+ //       {"Name":"Repeat", "start":25, "end": 60, "Info":"TNFR-Cys 1", "color":"black"},
+ //       {"Name":"Repeat", "start":61, "end": 103, "Info":"TNFR-Cys 2", "color":"black"},
+ //       {"Name":"Repeat", "start":104, "end": 144, "Info":"TNFR-Cys 3", "color":"black"},
+ //       {"Name":"Repeat", "start":145, "end": 187, "Info":"TNFR-Cys 4", "color":"black"}];
+
+        
+    // It's good practice to generate an svg container that is as large as we need.
+        var svgContainer = d3.select(place).append("svg")
+                                   .attr("width", 300)
+                                   .attr("height", 500);
+ 
+    // This binds the data in jsonRectangles_CD40 to the rectangles.
+        var rectangles = svgContainer.selectAll("rect")
+                            .data(molecule)
+                             .enter()
+                             .append("rect");
+                             
+    // This creates the rectangles using the bound data
+        var rectangleAttributes = rectangles
+                          .attr("x", 10)
+                          .attr("y", function (d) { return d.start; })
+                          .attr("height", function (d) { return (d.end - d.start); })
+                          .attr("width", 20)
+                          .style("fill",  function(d) { return d.color; });
+        
+    // add some text
+    //Add the SVG Text Element to the svgContainer
+        var text = svgContainer.selectAll("text")
+                        .data(molecule)
+                        .enter()
+                        .append("text");
+
+    //Add SVG Text Element Attributes
+        var textLabels = text
+                 .attr("x", 35)
+                 .attr("y", function (d) { return (d.start+(d.end-d.start)/2); })
+                 .text( function (d) { return d.Name + ", " + d.Info ; })
+                 .attr("font-family", "sans-serif")
+                 .attr("font-size", "10px")
+                 .attr("fill", "red");    
+        
+};
